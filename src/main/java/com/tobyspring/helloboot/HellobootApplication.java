c2ddd8392c07b6830415a33909e9321c7ef3debc
Jin_00 <anjdal64@gmail.com>
2024-02-08 23:36:39 +0900

    #6 - spring container + servlet container 통합, bean 등록
    Spring Container 와 Servlet Container 를 통합
    
    spring Container 에게 이 클래스를 등록하게끔, `@Configuration` 애노테이션과 register method 를 이용해서 명시해주었음
    
    `@Configuration` 애노테이션 안에 구성정보인 `@Bean` 애노테이션을 붙여서 등록해야할 빈 객체들을 등록해주었음

--- src/main/java/com/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/com/tobyspring/helloboot/HellobootApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -19,28 +23,43 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+// config 정보, 구성 정보를 가지고 있는 클래스라고 명시 ( spring container 가 이를 보고 bean object 를 만들면 되겠다고 인식함 )
+// configuration 이 붙은 클래스는 중요한 정보들을 많이 담고있다고 인식
+@Configuration
 public class HellobootApplication {
-
+	@Bean // bean object 등록
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+	
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
+	
 	public static void main(String[] args) {
-		// 스프링 컨테이너를 생성
-		GenericWebApplicationContext context = new GenericWebApplicationContext();
-		// bean 등록 ( 오브젝트를 넘기는 것이 아닌 클래스 정보만 넘김 )
-		context.registerBean(HelloController.class);
-		context.registerBean(SimpleHelloService.class);
+		// 스프링 컨테이너 + 서블릿 컨테이너 통합
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+				WebServer webServer = serverFactory.getWebServer(
+						servletContext -> {
+							servletContext
+									.addServlet("dispatcherServlet", new DispatcherServlet(this))
+									.addMapping("/*"); // frontcontroller 서블릿을 모든 요청에 매핑
+						}
+				);
+				webServer.start();
+			}
+		};
+		// spring container 에게 이 클래스를 등록하라고 명시
+		context.register(HellobootApplication.class);
 		// 컨테이너 초기화 ( bean object 생성 )
 		context.refresh();
-
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-
-		WebServer webServer = serverFactory.getWebServer(
-				servletContext -> {
-					servletContext
-							.addServlet("dispatcherServlet", new DispatcherServlet(context))
-							.addMapping("/*"); // frontcontroller 서블릿을 모든 요청에 매핑
-				}
-		);
-		webServer.start();
 
 	}
 
