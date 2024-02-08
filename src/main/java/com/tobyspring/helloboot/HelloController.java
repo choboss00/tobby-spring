67ce5e6d09fd607fd833ad6c347a2f91d1308f2a
Jin_00 <anjdal64@gmail.com>
2024-02-08 23:55:15 +0900

    #6 - Component 애노테이션 직접 만들기 및 적용
    애노테이션에 `@Component` 를 붙이더라도 잘 적용이 되는지 확인해보았음
    
    일반적으로 사용하는 애노테이션에도 역시 `@Component` 애노테이션이 붙어있기 때문에, 컴포넌트 스캔을 하고 가져온 클래스들을 DispatcherServlet 이 읽고 메소드들을 가져올 수 있게 됨

--- src/main/java/com/tobyspring/helloboot/HelloController.java
+++ src/main/java/com/tobyspring/helloboot/HelloController.java
@@ -1,5 +1,7 @@
 package com.tobyspring.helloboot;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Objects;
 // @RestController : dispatcherServlet 하고 직접적인 관련은 없음
 // @RestController 이 애노테이션을 붙이면, 명시적으로 @ResponseBody 를 붙이지 않아도 됨
-@RequestMapping("/hello") // 클래스 레벨에 걸어주면, dispatcherServlet 이 찾을 수 있음
+// 클래스 레벨에 걸어주면, dispatcherServlet 이 찾을 수 있음
+// @RequestMapping("/hello")
+// @MyComponent 직접 정의한 애노테이션 ( 애노테이션 위에 컴포넌트 애노테이션을 등록해도 잘 동작함 )
+@RestController
 public class HelloController {
     private final HelloService helloService;
 
@@ -19,8 +24,7 @@ public class HelloController {
     // dispatcherServlet 이 받을 수 있게끔 어노테이션을 걸어줌
     // method 레벨까지 다 뒤지는건 아니고, 클래스 레벨까지만 뒤짐
     // dispatcherServlet 은 view 를 찾는데, 없으니 404 에러를 뱉음. 그래서 애노테이션을 하나더 추가해줌
-    @GetMapping("")
-    @ResponseBody
+    @GetMapping("/hello")
     public String hello(String name) {
         // null 이 아닌 경우 그대로 리턴, null 인 경우 예외처리
         return helloService.sayHello(Objects.requireNonNull(name));
