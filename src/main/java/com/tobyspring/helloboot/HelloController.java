74bce1b13968f7d2fd21bf203d914b2bf3fd1a8a
Jin_00 <anjdal64@gmail.com>
2024-02-08 21:41:21 +0900

    #6 - 스프링 컨테이너를 사용하지 않은 채로, service 로직 위임

--- src/main/java/com/tobyspring/helloboot/HelloController.java
+++ src/main/java/com/tobyspring/helloboot/HelloController.java
@@ -1,8 +1,13 @@
 package com.tobyspring.helloboot;
 
+import java.util.Objects;
+
 public class HelloController {
 
     public String hello(String name) {
-        return "Hello, Spring Boot!" + name;
+
+        SimpleHelloService simpleHelloService = new SimpleHelloService();
+        // null 이 아닌 경우 그대로 리턴, null 인 경우 예외처리
+        return simpleHelloService.sayHello(Objects.requireNonNull(name));
     }
 }
