c2ae02c9c511b2c69641acff9ac66e31e2ef0aa0
Jin_00 <anjdal64@gmail.com>
2024-02-08 13:33:05 +0900

    #4 - frontcontroller 적용

--- src/main/java/com/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/com/tobyspring/helloboot/HellobootApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -24,18 +25,30 @@ public class HellobootApplication {
 		WebServer webServer = serverFactory.getWebServer(
 				servletContext -> {
 					// 서블릿 컨텍스트 초기화
-					servletContext.addServlet("hello", new HttpServlet() {
+					servletContext.addServlet("frontcontroller", new HttpServlet() {
 								@Override
 								protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-									String name = req.getParameter("name");
+									// 인증, 보안, 다국어 처리, 공통 기능 등등 처리를 앞에서 진행
+									// frontcontroller 가 매핑 기능을 담당해야 함
+									if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+										String name = req.getParameter("name");
 
-									// 상태 코드, 헤더 ( 컨텐츠 타입 헤더 ), 바디
-									resp.setStatus(HttpStatus.OK.value());
-									resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-									resp.getWriter().println("hello " + name);
+										// 상태 코드, 헤더 ( 컨텐츠 타입 헤더 ), 바디
+										resp.setStatus(HttpStatus.OK.value());
+										resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+										resp.getWriter().println("hello " + name);
+									}
+									else if (req.getRequestURI().equals("/user")) {
+										// ...
+									}
+									else {
+										resp.setStatus(HttpStatus.NOT_FOUND.value());
+									}
+
+
 								}
 							}
-					).addMapping("/hello"); // /hello 로 들어오면 여기서 처리하겠다!
+					).addMapping("/*"); // frontcontroller 서블릿을 모든 요청에 매핑
 				}
 		);
 		webServer.start();
