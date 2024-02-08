7d291fdd2b2d4512822bccce8be402857002c410
Jin_00 <anjdal64@gmail.com>
2024-02-08 13:18:26 +0900

    #4 - 서블릿 등록 후 응답값 만들기
    서블릿 컨테이너를 직접 하나 만든 후, 그 안에 서블릿을 하나 추가해보았음
    
    서블릿 컨테이너에 요청이 들어왔을 때, 어떤 서블릿에 이 작업을 할당해줄지 결정하는 매핑 과정을 직접 구현해봤음

--- src/main/java/com/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/com/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,40 @@
 package com.tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		// 추상화했기 때문에, 톰캣이 아닌 다른 WebServer 구현체로 변경할 수 있음
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		// getWebServer 는 TomcatWebServer 객체를 반환하는데, 이 객체는 WebServer 인터페이스를 상속받았기 때문에 추상화로 인해 받을 수 있음
+		WebServer webServer = serverFactory.getWebServer(
+				servletContext -> {
+					// 서블릿 컨텍스트 초기화
+					servletContext.addServlet("hello", new HttpServlet() {
+								@Override
+								protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+									// 상태 코드, 헤더 ( 컨텐츠 타입 헤더 ), 바디
+									resp.setStatus(200);
+									resp.setHeader("Content-Type", "text/plain");
+									resp.getWriter().println("hello, servlet world!");
+								}
+							}
+					).addMapping("/hello"); // /hello 로 들어오면 여기서 처리하겠다!
+				}
+		);
+		webServer.start();
+
 	}
 
 }
