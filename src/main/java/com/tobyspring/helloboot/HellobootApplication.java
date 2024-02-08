36da2733be62c882b238ec4cb36beda0c090e4f6
Jin_00 <anjdal64@gmail.com>
2024-02-08 13:57:48 +0900

    #6 - 스프링 컨테이너 생성
    `GenericApplicationContext` 라는 클래스를 활용해서 스프링 컨테이너를 만들어주었음
    
    그리고 이 스프링 컨테이너에 클래스 정보를 넘기면, 스프링 컨테이너 안에 bean 으로 등록이 됨
    
    이 bean 으로 등록된 클래스 정보는 내가 언제든지 꺼내서 쓰기만 하면 됨

--- src/main/java/com/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/com/tobyspring/helloboot/HellobootApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -19,13 +21,17 @@ import java.io.IOException;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		// 추상화했기 때문에, 톰캣이 아닌 다른 WebServer 구현체로 변경할 수 있음
+		// 스프링 컨테이너를 생성
+		GenericApplicationContext context = new GenericApplicationContext();
+		// bean 등록 ( 오브젝트를 넘기는 것이 아닌 클래스 정보만 넘김 )
+		context.registerBean(HelloController.class);
+		// 컨테이너 초기화 ( bean object 생성 )
+		context.refresh();
+
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		// getWebServer 는 TomcatWebServer 객체를 반환하는데, 이 객체는 WebServer 인터페이스를 상속받았기 때문에 추상화로 인해 받을 수 있음
+
 		WebServer webServer = serverFactory.getWebServer(
 				servletContext -> {
-					HelloController helloController = new HelloController();
-
 					// 서블릿 컨텍스트 초기화
 					servletContext.addServlet("frontcontroller", new HttpServlet() {
 								@Override
@@ -34,16 +40,14 @@ public class HellobootApplication {
 									// frontcontroller 가 매핑 기능을 담당해야 함 ( mapping )
 									if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 										String name = req.getParameter("name");
+
+										HelloController helloController = context.getBean(HelloController.class);
 										// binding : data 를 집어넣어서 처리하는 것
 										String ret = helloController.hello(name);
 
-										// 상태 코드, 헤더 ( 컨텐츠 타입 헤더 ), 바디
-										resp.setStatus(HttpStatus.OK.value());
-										resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+										// 상태 코드 ( 기본값 : 200 ), 헤더 ( 컨텐츠 타입 헤더 ), 바디
+										resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
 										resp.getWriter().println(ret);
-									}
-									else if (req.getRequestURI().equals("/user")) {
-										// ...
 									}
 									else {
 										resp.setStatus(HttpStatus.NOT_FOUND.value());
