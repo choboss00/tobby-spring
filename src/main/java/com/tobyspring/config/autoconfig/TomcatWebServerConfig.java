5b80c894a6cfa036499db615e564bcc0af9a2522
Jin_00 <anjdal64@gmail.com>
2024-02-09 23:26:09 +0900

    #14 - `@Value` 와 PropertySourcesPlaceholderConfigurer
    `@Value` 애노테이션을 이용해 엘리먼트로 치환자 ( placeholder ) 를 지정하고 컨테이너 초기화시 이를 프로퍼티 값으로 대체할 수 있음
    
    하지만 이 경우 PropertySourcesPlaceholderConfigurer 타입의 빈이 등록되어있어야 함
    
    이는 빈 팩토리의 후처리기로 동작하며, 초기 구성 정보에서 치환자 ( placeholder ) 를 찾아서 교체하는 역할을 담당함

--- src/main/java/com/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,6 +1,7 @@
 package com.tobyspring.config.autoconfig;
 
 import com.tobyspring.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -11,11 +12,14 @@ import org.springframework.core.env.Environment;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean(name = "tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+    public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 }
