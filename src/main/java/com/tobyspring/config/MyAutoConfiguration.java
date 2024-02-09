c1c924ad5165784f468ad3d82c05f727108a30dc
Jin_00 <anjdal64@gmail.com>
2024-02-09 17:44:39 +0900

    #10 - 자동 구성 애노테이션 적용

--- src/main/java/com/tobyspring/config/MyAutoConfiguration.java
+++ src/main/java/com/tobyspring/config/MyAutoConfiguration.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Configuration // meta-annotation
+@Configuration(proxyBeanMethods = false) // proxyBeanMethods 는 기본값이 true 이며, true 일 경우에는 @Bean 메소드를 호출할 때마다 새로운 프록시 객체를 생성하게 된다.
 public @interface MyAutoConfiguration {
 }
