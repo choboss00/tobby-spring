74b3e594167a55bebb420bc188deb78ae8360e47
Jin_00 <anjdal64@gmail.com>
2024-02-10 00:21:43 +0900

    #14 - 프로퍼티 빈 후처리기 도입
    프로퍼티 빈의 후처리기를 도입하여, 빈으로 등록된 클래스들의 애노테이션 정보들을 가져옴
    
    이 애노테이션이 내가 정의한 MyConfigurationProperties 일 경우, Binder 를 활용해서 필요한 값들을 주입해주고, 그렇지 않으면 그대로 bean 을 return 해줌
    
    그리고 `@Import` 애노테이션 안에 클래스 정보들을 담기 위한 Selector 클래스를 설정해주었음

--- src/main/java/com/tobyspring/config/autoconfig/ServerProperties.java
+++ src/main/java/com/tobyspring/config/autoconfig/ServerProperties.java
@@ -1,8 +1,11 @@
 package com.tobyspring.config.autoconfig;
 
+import com.tobyspring.config.MyConfigurationProperties;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-
+// 혼동을 피하기 위해 prefix 설정
+@MyConfigurationProperties(prefix = "server")
 public class ServerProperties {
 
     private String contextPath;
