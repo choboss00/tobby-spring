7865f311d0ed831a63e0a64286bdfbbf1d2da2a4
Jin_00 <anjdal64@gmail.com>
2024-02-09 16:07:07 +0900

    #10 - 합성 애노테이션 적용
    테스트 코드에서 합성 애노테이션을 적용하는 방법을 알았으니, 이제는 필요없어진 코드를 지워주었음
    
    합성 애노테이션을 활용해서 초기 Spring Initializr 에서 생성해주었던 초기 세팅과 유사하게 설정해줄 수 있었음

--- src/main/java/com/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/com/tobyspring/helloboot/HellobootApplication.java
@@ -10,21 +10,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-// config 정보, 구성 정보를 가지고 있는 클래스라고 명시 ( spring container 가 이를 보고 bean object 를 만들면 되겠다고 인식함 )
-// configuration 이 붙은 클래스는 중요한 정보들을 많이 담고있다고 인식
-@Configuration
-@ComponentScan // 이 패키지를 기준으로 하위 패키지를 모두 뒤져서 @Component 가 붙은 클래스를 찾아서 bean object 로 등록
+@MySpringBootAnnotation
 public class HellobootApplication {
-
-	@Bean
-	public ServletWebServerFactory servletWebServerFactory() {
-		return new TomcatServletWebServerFactory();
-	}
-	
-	@Bean
-	public DispatcherServlet dispatcherServlet() {
-		return new DispatcherServlet();
-	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HellobootApplication.class, args);
--- src/test/java/com/tobyspring/helloboot/HelloServiceTest.java
+++ src/test/java/com/tobyspring/helloboot/HelloServiceTest.java
@@ -8,23 +8,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-@UnitTest // UnitTest 는 메타 애노테이션으로 적용할 수 없음 -> @Target 을 추가해야함
-@interface FastUnitTest {
-
-}
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
-@Test // meta - annotation
-@interface UnitTest {
-
-}
-
 public class HelloServiceTest {
 
-    @UnitTest
+    @Test
     void simpleHelloTestService() {
         SimpleHelloService simpleHelloService = new SimpleHelloService();
 
