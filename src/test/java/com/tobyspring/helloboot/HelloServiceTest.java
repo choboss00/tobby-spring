c7479361dab5a1b7801a66b1ed47670a93f7f905
Jin_00 <anjdal64@gmail.com>
2024-02-09 15:58:51 +0900

    #10 - 직접 메타 애노테이션을 만든 후 테스트 진행
    메타 애노테이션이란, 애노테이션에 애노테이션을 붙이는 것
    
    이 애노테이션을 메타 애노테이션으로 사용하기 위해선, `@Target` 설정을 추가로 해줄 필요가 있음
    
    메타 애노테이션이 하나 이상 설정된 애노테이션을 합성 애노테이션 ( composed-annotation ) 이라고 함

--- src/test/java/com/tobyspring/helloboot/HelloServiceTest.java
+++ src/test/java/com/tobyspring/helloboot/HelloServiceTest.java
@@ -3,9 +3,28 @@ package com.tobyspring.helloboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest // UnitTest 는 메타 애노테이션으로 적용할 수 없음 -> @Target 을 추가해야함
+@interface FastUnitTest {
+
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Test // meta - annotation
+@interface UnitTest {
+
+}
+
 public class HelloServiceTest {
 
-    @Test
+    @UnitTest
     void simpleHelloTestService() {
         SimpleHelloService simpleHelloService = new SimpleHelloService();
 
