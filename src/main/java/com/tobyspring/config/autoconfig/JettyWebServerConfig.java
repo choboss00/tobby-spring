326aed032a0cc89750581a15ddf2c80ecf591726
Jin_00 <anjdal64@gmail.com>
2024-02-09 21:26:25 +0900

    #12 - 자동 구성 정보 대체하기
    AutoConfiguration 으로 등록되는 Tomcat Bean 을 ComponentScan 방식으로 대체했음
    
    이 경우 스프링 컨테이너에서 어떤 빈을 등록해야할지 모르기 때문에 에러가 발생함
    
    ComponentScan 이 먼저 일어나기 때문에, 스캔 후 빈이 없으면 생성이 되도록 하는 애노테이션인 `@ConditionalOnMissingBean` 을 붙여주었음

--- src/main/java/com/tobyspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/com/tobyspring/config/autoconfig/JettyWebServerConfig.java
@@ -1,6 +1,7 @@
 package com.tobyspring.config.autoconfig;
 
 import com.tobyspring.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -15,6 +16,7 @@ import org.springframework.util.ClassUtils;
 @ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     @Bean(name = "jettyWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
     }
--- src/main/java/com/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,20 +1,17 @@
 package com.tobyspring.config.autoconfig;
 
 import com.tobyspring.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean(name = "tomcatWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
