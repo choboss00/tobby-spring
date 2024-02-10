32e3036699e916713f8ac87bcd38f5f7181bbf61
Jin_00 <anjdal64@gmail.com>
2024-02-10 15:37:11 +0900

    #16 - Jdbc 탬플릿과 트랜잭션 매니저 구성

--- src/main/java/com/tobyspring/config/autoconfig/DataSourceConfig.java
+++ src/main/java/com/tobyspring/config/autoconfig/DataSourceConfig.java
@@ -4,8 +4,12 @@ import com.tobyspring.config.EnableMyConfigurationProperties;
 import com.tobyspring.config.MyAutoConfiguration;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import javax.xml.crypto.Data;
@@ -14,6 +18,7 @@ import java.sql.Driver;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
 @EnableMyConfigurationProperties(MyDataSourceProperties.class)
+@EnableTransactionManagement
 public class DataSourceConfig {
 
     @Bean
@@ -41,5 +46,20 @@ public class DataSourceConfig {
         dataSource.setPassword(properties.getPassword());
 
         return dataSource;
+    }
+
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class) // DataSoruce 타입의 하나의 빈만 존재할 때 가져와서 사용하겠다는 애노테이션
+    @ConditionalOnMissingBean
+    JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    // 이 빈을 등록하면 이제 @Transactional 애노테이션을 사용할 수 있게 된다.
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class) // DataSoruce 타입의 하나의 빈만 존재할 때 가져와서 사용하겠다는 애노테이션
+    @ConditionalOnMissingBean
+    JdbcTransactionManager jdbcTransactionManager(DataSource dataSource) {
+        return new JdbcTransactionManager(dataSource);
     }
 }
